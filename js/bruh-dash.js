bf0ce931a8be56cdeb28b75636f27c72ff47c4bc
Sarah <yamashige.sarah@gmail.com>
2018-06-12 21:32:07 -1000

    indexOf

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,18 +10,18 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+      return arr.shift();
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (arr) {
+      return arr.pop();
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr, num) {
+      return arr.indexOf(num);
   },
 
   // returns the index of the first matching element from right to left
